9179f8a65a5ec3931a8b5bdd30d7025cf9e0f1f8
Kyawlinnaung <kyawlinnaung@gmial.com>
2022-06-07 17:17:34 +0800

    i crated updatelistthing here

--- social/urls.py
+++ social/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns=[
           path('',views.list_thing,name="home_page"),
           path('list_retrieve/<str:pk>/',views.listthing_retrieve,name="listthing_retrieve"),
-          path('list_crate/',views.list_create,name="list_createpage"),
+          path('listcreate/',views.list_create,name="list_createpage"),
+          path('listupdate/<str:pk>/',views.list_update,name="list_updatepage"),
 ]
--- social/views.py
+++ social/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import listhing
 from .forms import ListFrom
 
@@ -22,7 +22,29 @@ def listthing_retrieve(request, pk):
 #start listcreateForm
 def list_create(request):
           form=ListFrom()
+           
+          if request.method == 'POST':
+                    form=ListFrom(request.POST)
+                    if form.is_valid():
+                              form.save()
+                              return redirect('home_page')
           context={'form':form}
           return render (request,'list_create.html',context)
 #end listcreateForm
+
+
+
+#start updatelist
+def list_update(request,pk):
+          listthings=listhing.objects.get(id=pk)
+          form=ListFrom(instance=listthings)
+          if request.method == 'POST':
+                    form=ListFrom(request.POST,instance=listthings)
+                    if form.is_valid():
+                              form.save()
+                              return redirect('home_page')
+          context={'form':form}
+          return render (request,'list_update.html',context)
+          
+#end updatelist
 
