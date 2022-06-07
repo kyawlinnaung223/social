4324bbcf145baf816c8a47606f28a4bf2c32b120
Kyawlinnaung <kyawlinnaung@gmial.com>
2022-06-07 16:03:09 +0800

    i crated list ceateform here

--- social/urls.py
+++ social/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-          path('',views.homepageviewpage,name="home_page"),
+          path('',views.list_thing,name="home_page"),
+          path('list_retrieve/<str:pk>/',views.listthing_retrieve,name="listthing_retrieve"),
+          path('list_crate/',views.list_create,name="list_createpage"),
 ]
--- social/views.py
+++ social/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-
+from .models import listhing
+from .forms import ListFrom
 
 # Create your views here.
 #start homepageview
-def homepageviewpage(request):
-          return render(request,'home.html')
+def list_thing(request):
+          listthings=listhing.objects.all()
+          context={'listthings':listthings}
+          return render(request,'home.html',context)
 #end homepageview
+
+
+#start listthing_retrieve
+def listthing_retrieve(request, pk):
+          listthings=listhing.objects.get(id=pk)
+          context={'listthings':listthings}
+         
+          return render(request,'listing_retrieve.html',context)
+#end listthing_retrieve
+
+#start listcreateForm
+def list_create(request):
+          form=ListFrom()
+          context={'form':form}
+          return render (request,'list_create.html',context)
+#end listcreateForm
+
