063f09019f6ff71a126ae6e2608794018bd8985d
Kyawlinn <kyawlinn@email.com>
2022-06-16 17:11:53 +0630

    I created my homepage template here

--- social/urls.py
+++ social/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from .import views
 
 urlpatterns=[
-          path('',views.list_thing,name="home_page"),
-          path('list_retrieve/<str:pk>/',views.listthing_retrieve,name="listthing_retrieve"),
-          path('listcreate/',views.list_create,name="list_createpage"),
-          path('listupdate/<str:pk>/',views.list_update,name="list_updatepage"),
+    path('',views.homepage,name="homepage"),
 ]
--- social/views.py
+++ social/views.py
@@ -1,50 +1,9 @@
 from django.shortcuts import render,redirect
-from .models import listhing
-from .forms import ListFrom
 
-# Create your views here.
-#start homepageview
-def list_thing(request):
-          listthings=listhing.objects.all()
-          context={'listthings':listthings}
-          return render(request,'home.html',context)
+
+#home pageview
+def homepage(request):
+    return render(request,'home.html')
 #end homepageview
 
-
-#start listthing_retrieve
-def listthing_retrieve(request, pk):
-          listthings=listhing.objects.get(id=pk)
-          context={'listthings':listthings}
-         
-          return render(request,'listing_retrieve.html',context)
-#end listthing_retrieve
-
-#start listcreateForm
-def list_create(request):
-          form=ListFrom()
-           
-          if request.method == 'POST':
-                    form=ListFrom(request.POST)
-                    if form.is_valid():
-                              form.save()
-                              return redirect('home_page')
-          context={'form':form}
-          return render (request,'list_create.html',context)
-#end listcreateForm
-
-
-
-#start updatelist
-def list_update(request,pk):
-          listthings=listhing.objects.get(id=pk)
-          form=ListFrom(instance=listthings)
-          if request.method == 'POST':
-                    form=ListFrom(request.POST,instance=listthings)
-                    if form.is_valid():
-                              form.save()
-                              return redirect('home_page')
-          context={'form':form}
-          return render (request,'list_update.html',context)
-          
-#end updatelist
 
